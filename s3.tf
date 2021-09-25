80cbfa56236dbf78ce46d45258a3510c8c723648
Ashutosh <ashu.singh212@gmail.com>
2021-09-26 00:40:57 +0530

    fix: added logs enabled

--- s3.tf
+++ s3.tf
@@ -1,7 +1,18 @@
+resource "aws_s3_bucket" "log" {
+  bucket = "kode-ark-access-log"
+  tags = {
+    Name = "Server Access Log Bucket"
+  }
+}
+
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket
   tags = {
     Name = "Kode Ark Web Application"
   }
-  #  acl = "public-read"
+    acl = "public-read"
+  logging {
+    target_bucket = aws_s3_bucket.log.id
+    target_prefix = "log"
+  }
 }
